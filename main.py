c8ef7a247319f59b2c7831c7d7645fb0baaf5697
nuhla <nuhlamasri@gmail.com>
2021-01-02 22:22:00 +0200

    create my first report

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from Forms import RegistrationForm , LoginForm
 from flask import request
 from flask import Flask, redirect, url_for, request
-import pyrebase
+from firebase import Firebase
+import pdfkit
 
 config = {
     "apiKey": "AIzaSyC_16qNyYweahPpI08fq5PU4scSTSkmwT4",
@@ -14,9 +15,8 @@ config = {
   "appId": "1:36855175985:web:e8f30719f027fe2234ee10",
   "measurementId": "G-PS94E533ME"
 }
-firebase = pyrebase.initialize_app(config)
-db=firebase.database()
-print(db.get())
+firebase = Firebase(config)
+db = firebase.database()
 
 data=[]
 
@@ -29,6 +29,12 @@ app.config['SECRET_KEY']='9fd51d0c027fb09ad5a12825b68b4457'
 
 @app.route('/')
 def Home():
+    db=firebase.database()
+    
+    # print(db.child("locations"), "my datasroe")
+    # db.child("ProductMovement").push({"timestamp":"Nov 13 2020", "from_location":"nabluse", "to_location":"","product_id":"table", "qyt":165 })
+    # data2= db.child("locations").get()
+    # print(db.ref())
     return render_template('HomePage.html', data=data, title="homepage")
 def hello_world():
     return 'Hello, World!'
@@ -57,6 +63,18 @@ def LogIn():
     form= LoginForm()
     return render_template('login.html', title="LogIn", form=form )
     pass
+
+
+@app.route('/Reports',methods = ['POST', 'GET'])
+def PDF_Template():
+    render = render_template('pdfReport.html')
+    pdf = pdfkit.from_string(render, False)
+    response = make_response(pdf)
+    response.headers['Content-Type']='application/pdf'
+    response.headers['Content-Disposition']='inline; filename=output.pdf'
+    
+    return response
+    
 
 if __name__ == '__main__':
    app.run(debug = True)
